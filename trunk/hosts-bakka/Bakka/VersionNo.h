f1fe57833966eedd697bee722b18379ce0d2128c
likejazz <likejazz@6b461e02-38ac-11de-a81c-fde7d73ceb89>
2009-05-15 08:26:18 +0000

    Add Version Autoning

---
+++ trunk/hosts-bakka/Bakka/VersionNo.h
@@ -0,0 +1,29 @@
+/*-----------------------------------------------------------
+  VERSION CONTROL BUILD SYSTEM                               
+  This header file was created by VERBUILD v1.0.1            
+  -----------------------------------------------------------
+  help : verbuild -?                                         
+  info : http://www.yeamaec.com                              
+         yeamaec@hanafos.com ,krkim@yeamaec.com              
+-----------------------------------------------------------*/
+
+#ifndef VERSIONNO__H
+#define VERSIONNO__H
+
+#define VERSION_FULL           2.0.134.6
+
+#define VERSION_BASEYEAR       2009
+#define VERSION_DATE           "2009-05-15"
+#define VERSION_TIME           "17:24:37"
+
+#define VERSION_MAJOR          2
+#define VERSION_MINOR          0
+#define VERSION_BUILDNO        134
+#define VERSION_EXTEND         6
+
+#define VERSION_FILE           2,0,134,6
+#define VERSION_PRODUCT        2,0,134,6
+#define VERSION_FILESTR        "2,0,134,6\0"
+#define VERSION_PRODUCTSTR     "2,0,134,6\0"
+
+#endif
