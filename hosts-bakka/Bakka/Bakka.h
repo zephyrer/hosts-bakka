b96fc404b59be29aeccf268f9ac857e680fdbfa6
likejazz <likejazz@6b461e02-38ac-11de-a81c-fde7d73ceb89>
2009-08-18 07:19:50 +0000

    Add INI configuration.

--- hosts-bakka/Bakka/Bakka.h
+++ hosts-bakka/Bakka/Bakka.h
@@ -15,14 +15,6 @@
 #define BAKKA_START_TAG		L"## BAKKA_START: DO NOT MODIFY\n"
 #define BAKKA_END_TAG		L"\n## BAKKA_END: DO NOT MODIFY"
 
-// Define your hosting server, files and port
-#define HOST_SERVER			L""
-#define HOST_FILE1			L""
-#define HOST_FILE2			L""
-#define HOST_FILE3			L""
-#define HOST_FILE4			L""
-#define HOST_PORT			80
-
 #ifndef __AFXWIN_H__
 	#error "include 'stdafx.h' before including this file for PCH"
 #endif
