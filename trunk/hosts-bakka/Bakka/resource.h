5a881ea3dd5c527c6ecfa80bd713ea4b9396e86a
likejazz <likejazz@6b461e02-38ac-11de-a81c-fde7d73ceb89>
2009-08-21 06:15:35 +0000

    . Clear resource file names.

--- trunk/hosts-bakka/Bakka/resource.h
+++ trunk/hosts-bakka/Bakka/resource.h
@@ -8,14 +8,12 @@
 #define IDD_BAKKA_DIALOG                102
 #define IDR_MAINFRAME                   128
 #define IDR_POPUP_MENU                  130
-#define IDC_LIST1                       1000
-#define IDC_BUTTON1                     1001
-#define IDC_BUTTON5                     1001
-#define IDC_BUTTON4                     1002
 #define IDC_PROGRESS1                   1006
 #define IDC_EDIT1                       1008
 #define IDC_BUTTON2                     1009
 #define IDC_BUTTON3                     1010
+#define IDC_BUTTON4                     1002
+#define IDC_BUTTON5                     1001
 
 // Next default values for new objects
 // 
